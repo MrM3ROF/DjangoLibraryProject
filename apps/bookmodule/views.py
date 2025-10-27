a28ec9304956356b3712fb1fe35de5744f6ce904
Mohammed Mansour <7akim.ita@gamil.com>
2025-10-27 12:36:12 +0300

    Lab 7 Finished

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book
+
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
+
 
 def index(request):
       return render(request, "bookmodule/index.html")
@@ -25,6 +33,36 @@ def listing(request):
 def tables(request):
     return render(request, "bookmodule/tables.html")
 
+#def search(request):
+#    return render(request,"bookmodule/search.html")
+
+def search(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower():contained = True
+            if contained: newBooks.append(item)
+        return render(request, 'bookmodule/bookList.html', {'books':newBooks})
+    return render(request,"bookmodule/search.html")
+         
+def simple_query(request):
+    mybooks=Book.objects.filter(title__icontains='a') 
+    return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+
+
+def lookup_query(request):
+    mybooks=books=Book.objects.filter(author__isnull =
+False).filter(title__icontains='a').filter(edition__gte = 2).exclude(price__lte = 10)[:10]
+    if len(mybooks)>=1:
+        return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+    else:
+        return render(request, 'bookmodule/index.html')
 
 
 # def index(request):
